ba355b9aa10ad4a8d6c565a564838172cfb49233
Nelson Miranda <nmiranda@slc.hn>
2023-12-16 13:37:19 -0600

    Nested views

---
+++ 15_nested_views/webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (
+    Controller,
+    MessageToast
+) {
+    "use strict";
+    return Controller.extend("n.miranda.ui.controller.HelloPanel", {
+        onSayHiButton(oEvent) {
+            // read msg from i18n model
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+            // show message
+            MessageToast.show(sMsg);
+        }
+    });
+});
