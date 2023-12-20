0c321da0b987275be3db0f1f2e7428c25163041f
Nelson Miranda <nmiranda@slc.hn>
2023-12-20 16:37:45 -0600

    Unit Test with QUnit

---
+++ 27_unit_test_with_qunit/webapp/Component.js
@@ -0,0 +1,27 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel"
+], (UIComponent, JSONModel) => {
+	"use strict";
+
+	return UIComponent.extend("ui5.walkthrough.Component", {
+		metadata: {
+			interfaces: ["sap.ui.core.IAsyncContentCreation"],
+			manifest: "json"
+		},
+
+		init() {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set data model on view
+			const oData = {
+				recipient: {
+					name: "World"
+				}
+			};
+			const oModel = new JSONModel(oData);
+			this.setModel(oModel);
+		}
+	});
+});
