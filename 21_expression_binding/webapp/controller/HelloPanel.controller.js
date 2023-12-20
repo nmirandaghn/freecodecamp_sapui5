eb0a900cddd147eb1d1c1946dd3945f47921e336
Nelson Miranda <nmiranda@slc.hn>
2023-12-20 00:30:12 -0600

    Expression binding

---
+++ 21_expression_binding/webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,34 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (
+    Controller,
+    MessageToast
+) {
+    "use strict";
+    return Controller.extend("n.miranda.ui.controller.HelloPanel", {
+        onSayHiButton(oEvent) {
+            // read msg from i18n model
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+            // show message
+            MessageToast.show(sMsg);
+        },
+        onOpenDialog() {
+            // create dialog lazily
+            this.pDialog ??= this.loadFragment({
+                name: "n.miranda.ui.view.HelloDialog"
+            });
+
+            this.pDialog.then((oDialog) => oDialog.open());
+        },
+
+		onCloseDialog() {
+			// note: We don't need to chain to the pDialog promise, since this event handler
+			// is only called from within the loaded dialog itself.
+			this.byId("helloDialog").close();
+		}
+    });
+});
---
+++ 21_expression_binding/webapp/controller/InvoiceList.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
+    "use strict";
+
+    return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
+        onInit() {
+            const oViewModel = new JSONModel({
+                currency: "EUR"
+            });
+            this.getView().setModel(oViewModel, "view");
+        }
+    });
+});
