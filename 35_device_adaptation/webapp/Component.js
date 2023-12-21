be57fa4b6ad2993c9070a0dbf9efb48e2087b642
Nelson Miranda <nmiranda@slc.hn>
2023-12-21 09:42:20 -0600

    Device adaptation

---
+++ 35_device_adaptation/webapp/Component.js
@@ -0,0 +1,36 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/Device"
+], (UIComponent, JSONModel, Device) => {
+	"use strict";
+
+	return UIComponent.extend("ui5.walkthrough.Component", {
+		metadata: {
+			interfaces: ["sap.ui.core.IAsyncContentCreation"],
+			manifest: "json"
+		},
+
+		init() {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set data model on view
+			const oData = {
+				recipient: {
+					name: "World"
+				}
+			};
+			const oModel = new JSONModel(oData);
+			this.setModel(oModel);
+
+			// set device model
+			const oDeviceModel = new JSONModel(Device);
+			oDeviceModel.setDefaultBindingMode("OneWay");
+			this.setModel(oDeviceModel, "device");
+
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+		}
+	});
+});
